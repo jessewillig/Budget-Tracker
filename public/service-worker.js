3e12d62ca616b227bcd937159011a9f72c50b58d
Jesse Willig <jessewillig@outlook.com>
2021-04-25 16:28:24 -0700

    added service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
@@ -23,4 +25,48 @@ self.addEventListener("install", function (event) {
         })
     );
     self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    const {url} = event.request;
+    if (url.includes("/all") || url.includes("/find")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+    } else {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(cache => {
+                return cache.match(event.request).then(response => {
+                    return response || fetch(event.request);
+                });
+            })
+        );
+    }
 });
