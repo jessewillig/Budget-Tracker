6a1ce3d5b5922e849121ba16ca4e96ed110141a8
Jesse Willig <jessewillig@outlook.com>
2021-04-25 16:54:24 -0700

    small syntax change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -29,6 +29,10 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
